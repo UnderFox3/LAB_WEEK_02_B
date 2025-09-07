322f90dac8e8da48d4d116c014cd298821552887
Activity_Lifecycle <natan.aw95@gmail.com>
2025-09-08 06:14:17 +0700

    Commit No. 3 - Fix application crash when user input wrong color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,10 +14,20 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     private val submitButton : Button
         get() = findViewById(R.id.submit_button)
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        activityResult ->
+        val data = activityResult.data
+        val error = data?.getBooleanExtra(ERROR_KEY, false)
+        if(error == true) {
+            Toast.makeText(this, getString(R.string.color_code_input_invalid), Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     val resultIntent = Intent(this, Result_Activity::class.java)
                     resultIntent.putExtra(COLOR_KEY, colorCode)
-                    startActivity(resultIntent)
+                    startForResult.launch(resultIntent)
                 }
             } else {
                 Toast.makeText(this, getString(R.string.color_code_input_empty), Toast.LENGTH_LONG).show()
